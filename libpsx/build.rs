6fbba271d3b4b63165348695713b4da3c4fa9ed4
Ayrton <a.munoz3327@gmail.com>
2020-10-26 17:50:41 -0400

    Temporarily removed some bios functions and cleaned up build script

--- libpsx/build.rs
+++ libpsx/build.rs
@@ -3,20 +3,23 @@ use std::process::Command;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
+    let mips_dir = "../mips_toolchain";
+    println!("cargo:rustc-link-search={}", out_dir);
     let asm_files = ["bios", "load_delay_slot"];
     for f in &asm_files {
         let src_file = &format!("src/{}.s", f);
         let obj_file = &format!("{}/{}.o", out_dir, f);
         let ar_file = &format!("{}/lib{}.a", out_dir, f);
-        Command::new("../mips_toolchain/as")
+        let as_bin = &format!("{}/as", mips_dir);
+        let ar_bin = &format!("{}/ar", mips_dir);
+        Command::new(as_bin)
                 .args(&["-O2", "-o", obj_file, src_file])
                 .status()
                 .unwrap();
-        Command::new("../mips_toolchain/ar")
+        Command::new(ar_bin)
                 .args(&["rcs", ar_file, obj_file])
                 .status()
                 .unwrap();
-        println!("cargo:rustc-link-search={}", out_dir);
         println!("cargo:rustc-link-lib=static={}", f);
     }
 }
--- libpsx/src/bios.rs
+++ libpsx/src/bios.rs
@@ -1,8 +1,4 @@
 extern {
-    fn asm_putchar(b: u8) -> u32;
-    fn asm_puts(s: *const u8) -> u32;
-    fn asm_toupper(b: u8) -> u8;
-    fn asm_print_devices();
     fn asm_printf(s: *const u8, v: u32);
     fn asm_gpu_get_status() -> u32;
     fn asm_gpu_gp1_command_word(cmd: u32);
@@ -10,20 +6,10 @@ extern {
     fn asm_gpu_command_word_and_params(src: *const u32, num: u32);
 }
 
-fn print_devices() {
+pub fn printf(c: *const u8, v: u32) {
     unsafe {
-        asm_print_devices();
+        asm_printf(c, v);
     }
-}
-
-fn putchar(c: u8) {
-    unsafe {
-        asm_putchar(c);
-    }
-}
-
-fn printf(c: *const u8, v: u32) {
-    unsafe { asm_printf(c, v) };
 }
 
 pub fn gpu_gp1_command_word(cmd: u32) {
@@ -39,5 +25,10 @@ pub fn gpu_command_word(cmd: u32) {
 pub fn gpu_command_word_and_params(src: *const u32, num: u32) {
     unsafe {
         asm_gpu_command_word_and_params(src, num);
+    }
+}
+pub fn gpu_get_status() -> u32 {
+    unsafe {
+        asm_gpu_get_status()
     }
 }
