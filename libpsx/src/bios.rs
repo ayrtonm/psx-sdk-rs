6c57f9510b90928610d17b7bade3f5efa562bdd1
Ayrton <a.munoz3327@gmail.com>
2020-10-26 17:36:55 -0400

    Renamed bios asm functions

--- libpsx/src/bios.rs
+++ libpsx/src/bios.rs
@@ -1,43 +1,43 @@
 extern {
-    fn bios_putchar(b: u8) -> u32;
-    fn bios_puts(s: *const u8) -> u32;
-    fn bios_toupper(b: u8) -> u8;
-    fn bios_print_devices();
-    fn bios_printf(s: *const u8, v: u32);
-    fn bios_gpu_get_status() -> u32;
-    fn bios_gpu_gp1_command_word(cmd: u32);
-    fn bios_gpu_command_word(cmd: u32);
-    fn bios_gpu_command_word_and_params(src: *const u32, num: u32);
+    fn asm_putchar(b: u8) -> u32;
+    fn asm_puts(s: *const u8) -> u32;
+    fn asm_toupper(b: u8) -> u8;
+    fn asm_print_devices();
+    fn asm_printf(s: *const u8, v: u32);
+    fn asm_gpu_get_status() -> u32;
+    fn asm_gpu_gp1_command_word(cmd: u32);
+    fn asm_gpu_command_word(cmd: u32);
+    fn asm_gpu_command_word_and_params(src: *const u32, num: u32);
 }
 
 fn print_devices() {
     unsafe {
-        bios_print_devices();
+        asm_print_devices();
     }
 }
 
 fn putchar(c: u8) {
     unsafe {
-        bios_putchar(c);
+        asm_putchar(c);
     }
 }
 
 fn printf(c: *const u8, v: u32) {
-    unsafe { bios_printf(c, v) };
+    unsafe { asm_printf(c, v) };
 }
 
 pub fn gpu_gp1_command_word(cmd: u32) {
     unsafe {
-        bios_gpu_gp1_command_word(cmd);
+        asm_gpu_gp1_command_word(cmd);
     }
 }
 pub fn gpu_command_word(cmd: u32) {
     unsafe {
-        bios_gpu_command_word(cmd);
+        asm_gpu_command_word(cmd);
     }
 }
 pub fn gpu_command_word_and_params(src: *const u32, num: u32) {
     unsafe {
-        bios_gpu_command_word_and_params(src, num);
+        asm_gpu_command_word_and_params(src, num);
     }
 }
--- libpsx/src/bios.s
+++ libpsx/src/bios.s
@@ -5,66 +5,66 @@
 
 .text
 
-.section .text.bios_printf
-.global bios_printf
-.type bios_printf, function
+.section .text.asm_printf
+.global asm_printf
+.type asm_printf, function
 
-bios_printf:
+asm_printf:
 	j  0xa0
 	li $t1, 0x3f
 
-.section .text.bios_flush_cache
-.global bios_flush_cache
-.type bios_flush_cache, function
+.section .text.asm_flush_cache
+.global asm_flush_cache
+.type asm_flush_cache, function
 
-bios_flush_cache:
+asm_flush_cache:
 	j  0xa0
 	li $t1, 0x44
 
-.section .text.bios_gpu_sync
-.global bios_gpu_sync
-.type bios_gpu_sync, function
+.section .text.asm_gpu_sync
+.global asm_gpu_sync
+.type asm_gpu_sync, function
 
-bios_gpu_sync:
+asm_gpu_sync:
 	j  0xa0
 	li $t1, 0x4e
 
-.section .text.bios_gpu_command_word
-.global bios_gpu_command_word
-.type bios_gpu_command_word, function
+.section .text.asm_gpu_command_word
+.global asm_gpu_command_word
+.type asm_gpu_command_word, function
 
-bios_gpu_command_word:
+asm_gpu_command_word:
 	j  0xa0
 	li $t1, 0x49
 
-.section .text.bios_gpu_command_word_and_params
-.global bios_gpu_command_word_and_params
-.type bios_gpu_command_word_and_params, function
+.section .text.asm_gpu_command_word_and_params
+.global asm_gpu_command_word_and_params
+.type asm_gpu_command_word_and_params, function
 
-bios_gpu_command_word_and_params:
+asm_gpu_command_word_and_params:
 	j  0xa0
 	li $t1, 0x4a
 
-.section .text.bios_gpu_gp1_command_word
-.global bios_gpu_gp1_command_word
-.type bios_gpu_gp1_command_word, function
+.section .text.asm_gpu_gp1_command_word
+.global asm_gpu_gp1_command_word
+.type asm_gpu_gp1_command_word, function
 
-bios_gpu_gp1_command_word:
+asm_gpu_gp1_command_word:
 	j  0xa0
 	li $t1, 0x48
 
-.section .text.bios_gpu_get_status
-.global bios_gpu_get_status
-.type bios_gpu_get_status, function
+.section .text.asm_gpu_get_status
+.global asm_gpu_get_status
+.type asm_gpu_get_status, function
 
-bios_gpu_get_status:
+asm_gpu_get_status:
 	j  0xa0
 	li $t1, 0x4d
 
-.section .text.bios_kernel_redirect
-.global bios_kernel_redirect
-.type bios_kernel_redirect, function
+.section .text.asm_kernel_redirect
+.global asm_kernel_redirect
+.type asm_kernel_redirect, function
 
-bios_kernel_redirect:
+asm_kernel_redirect:
 	j  0xc0
 	li $t1, 0x1b
