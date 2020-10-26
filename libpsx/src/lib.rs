a77aaae795e67eb7804ee0c3191dd60c10bca260
Ayrton <a.munoz3327@gmail.com>
2020-10-26 11:29:59 -0400

    Added memcpy to libpsx so it'll work with cargo's build-std=core

--- libpsx/src/lib.rs
+++ libpsx/src/lib.rs
@@ -1,5 +1,8 @@
 #![feature(lang_items,asm,core_intrinsics)]
 #![no_std]
+// This library defines the builtin functions, so it would be a shame for
+// LLVM to optimize these function calls to themselves!
+#![no_builtins]
 #![crate_type = "rlib"]
 #![crate_name = "psx"]
 
@@ -14,6 +17,17 @@ pub extern fn memset(dst: *mut u8, b: i32, len: u32) -> *mut u8 {
     }
 
     dst
+}
+
+#[no_mangle]
+pub unsafe extern fn memcpy(dest: *mut u8, src: *const u8,
+                            n: usize) -> *mut u8 {
+    let mut i = 0;
+    while i < n {
+        *dest.offset(i as isize) = *src.offset(i as isize);
+        i += 1;
+    }
+    return dest;
 }
 
 // Various lang items required by rustc
