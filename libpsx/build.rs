0a26b6d1411ed0b67c6f83224495edd51a287028
Ayrton <a.munoz3327@gmail.com>
2020-10-26 17:40:52 -0400

    Used $OUT_DIR in build script to make cleanup easier

--- libpsx/build.rs
+++ libpsx/build.rs
@@ -1,11 +1,13 @@
+use std::env;
 use std::process::Command;
 
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
     let asm_files = ["bios", "load_delay_slot"];
     for f in &asm_files {
         let src_file = &format!("src/{}.s", f);
-        let obj_file = &format!("{}.o", f);
-        let ar_file = &format!("lib{}.a", f);
+        let obj_file = &format!("{}/{}.o", out_dir, f);
+        let ar_file = &format!("{}/lib{}.a", out_dir, f);
         Command::new("../mips_toolchain/as")
                 .args(&["-O2", "-o", obj_file, src_file])
                 .status()
@@ -14,7 +16,7 @@ fn main() {
                 .args(&["rcs", ar_file, obj_file])
                 .status()
                 .unwrap();
-        println!("cargo:rustc-link-search=./");
+        println!("cargo:rustc-link-search={}", out_dir);
         println!("cargo:rustc-link-lib=static={}", f);
     }
 }
