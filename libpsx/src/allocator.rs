c2bee2844f95b0caa1d9bf29bd0f0245e2c6ccf8
Ayrton <a.munoz3327@gmail.com>
2020-11-06 00:14:57 -0400

    Added some TODO comments to libpsx::allocator

--- libpsx/src/allocator.rs
+++ libpsx/src/allocator.rs
@@ -10,9 +10,23 @@ impl BiosAllocator {
     }
 }
 
+// TODO: Make this impl interrupt-free as seen here
+// https://docs.rust-embedded.org/book/collections/
+// This'll probably require some small bits of assembly and glue to modify interrupts
 unsafe impl GlobalAlloc for BiosAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        bios::malloc(layout.size())
+        let ptr = bios::malloc(layout.size());
+        // TODO: test for errors
+        //use crate::gpu::position::*;
+        //use crate::gpu::color::*;
+        //use crate::gpu::polygon::*;
+        //if ptr == core::ptr::null_mut() {
+        //    draw_square(&Position::zero(), 16, &Color::red(), &Opacity::Opaque);
+        //} else {
+        //    let p = Position::new(16,0);
+        //    draw_square(&p, 16, &Color::green(), &Opacity::Opaque);
+        //};
+        ptr
     }
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
         bios::free(ptr)
