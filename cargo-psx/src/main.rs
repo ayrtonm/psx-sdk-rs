c0bbac52cca921a3e68b08721df4bb266d0a1f37
Ayrton <a.munoz3327@gmail.com>
2020-11-12 11:19:57 -0400

    Added another function for handling cargo-psx-specific flags
    This new function extracts flags without a value (e.g. cargo psx --skip-build)

--- cargo-psx/src/main.rs
+++ cargo-psx/src/main.rs
@@ -1,21 +1,45 @@
 #![feature(bool_to_option)]
 
+use cargo_metadata::MetadataCommand;
 use std::env;
 use std::process::{self, Command, Stdio};
-use cargo_metadata::MetadataCommand;
 
-fn extract_flag(key: &str, args: Vec<String>) -> (Option<String>, Vec<String>) {
+fn extract_flag(flag: &str, args: Vec<String>) -> (bool, Vec<String>) {
+    let flag_present = args.iter().cloned().filter(|arg| arg == flag).count() == 1;
+    let args = if flag_present {
+        args.split(|arg| arg == flag)
+            .flatten()
+            .cloned()
+            .collect::<Vec<String>>()
+    } else {
+        args
+    };
+    (flag_present, args)
+}
+fn extract_key_value(key: &str, args: Vec<String>) -> (Option<String>, Vec<String>) {
     // Splits arguments at key argument
     let mut temp_iter = args.split(|arg| arg == key);
     // Gets all arguments before key
-    let cargo_args = temp_iter.next().unwrap().iter().cloned().collect::<Vec<String>>();
-    // Gets arguments after key if any and pops the first argument after key, i.e. the
-    // desired key's value. Then combines the remaining arguments with the ones before key.
+    let cargo_args = temp_iter
+        .next()
+        .unwrap()
+        .iter()
+        .cloned()
+        .collect::<Vec<String>>();
+    // Gets arguments after key if any and pops the first argument after key, i.e.
+    // the desired key's value. Then combines the remaining arguments with the
+    // ones before key.
     match temp_iter.next() {
         Some(v) => {
             let mut it = v.iter();
-            (Some(it.next().unwrap().to_string()),
-            cargo_args.iter().chain(it).cloned().collect::<Vec<String>>())
+            (
+                Some(it.next().unwrap().to_string()),
+                cargo_args
+                    .iter()
+                    .chain(it)
+                    .cloned()
+                    .collect::<Vec<String>>(),
+            )
         },
         None => (None, cargo_args),
     }
@@ -24,14 +48,9 @@ fn extract_flag(key: &str, args: Vec<String>) -> (Option<String>, Vec<String>) {
 fn main() {
     // Skips `cargo psx`
     let args = env::args().skip(2).collect::<Vec<String>>();
-    let (region, mut cargo_args) = extract_flag("--region", args);
+    let (region, mut cargo_args) = extract_key_value("--region", args);
     let region = region.unwrap_or("NA".to_string());
-    let skip_key = "--skip-build";
-    let skip_build = cargo_args.iter().cloned().filter(|arg| arg == skip_key).count() == 1;
-    if skip_build {
-        cargo_args = cargo_args.split(|arg| arg == skip_key).flatten().cloned().collect::<Vec<String>>();
-    }
-
+    let (skip_build, cargo_args) = extract_flag("--skip-build", cargo_args);
 
     let target_triple = "mipsel-sony-psx";
     if !skip_build {
@@ -58,10 +77,11 @@ fn main() {
         }
     }
 
-    let metadata = MetadataCommand::new()
-        .exec()
-        .unwrap();
-    let profile = env::args().any(|arg| arg == "--release").then_some("release").unwrap_or("debug");
+    let metadata = MetadataCommand::new().exec().unwrap();
+    let profile = env::args()
+        .any(|arg| arg == "--release")
+        .then_some("release")
+        .unwrap_or("debug");
 
     let target_dir = metadata.target_directory.join(target_triple).join(profile);
     for pkg in metadata.packages {
