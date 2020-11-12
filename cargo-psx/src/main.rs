245aad291cea53f76c761f85cf42c78b56332b76
Ayrton <a.munoz3327@gmail.com>
2020-11-12 10:39:07 -0400

    Added --skip-build to cargo-psx and moved option extraction to a separate function
    This will make it much easier to add flags to cargo-psx without interfering with
    cargo build flags.

--- cargo-psx/src/main.rs
+++ cargo-psx/src/main.rs
@@ -4,46 +4,57 @@ use std::env;
 use std::process::{self, Command, Stdio};
 use cargo_metadata::MetadataCommand;
 
+fn extract_option(args: Vec<String>, key: &str) -> (Option<String>, Vec<String>) {
+    // Splits arguments at key argument
+    let mut temp_iter = args.split(|arg| arg == key);
+    // Gets all arguments before key
+    let cargo_args = temp_iter.next().unwrap().iter().cloned().collect::<Vec<String>>();
+    // Gets arguments after key if any and pops the first argument after key, i.e. the
+    // desired key's value. Then combines the remaining arguments with the ones before key.
+    match temp_iter.next() {
+        Some(v) => {
+            let mut it = v.iter();
+            (Some(it.next().unwrap().to_string()),
+            cargo_args.iter().chain(it).cloned().collect::<Vec<String>>())
+        },
+        None => (None, cargo_args),
+    }
+}
+
 fn main() {
     // Skips `cargo psx`
     let args = env::args().skip(2).collect::<Vec<String>>();
+    let (region, mut cargo_args) = extract_option(args, "--region");
+    let region = region.unwrap_or("NA".to_string());
+    let skip_build = cargo_args.iter().cloned().filter(|arg| arg == "--skip-build").count() == 1;
+    if skip_build {
+        cargo_args = cargo_args.split(|arg| arg == "--skip-build").flatten().cloned().collect::<Vec<String>>();
+    }
 
-    // Splits arguments at `--region` argument
-    let mut temp_iter = args.split(|arg| arg == "--region");
-    // Gets all arguments before `--region`
-    let cargo_args = temp_iter.next().unwrap().iter().cloned().collect::<Vec<String>>();
-    // Gets arguments after `--region` if any and pops the first argument after `--region`, i.e. the
-    // desired region. Then combines the remaining arguments with the ones before `--region`.
-    let (region, cargo_args) = match temp_iter.next() {
-        Some(v) => {
-            let mut it = v.iter();
-            (it.next().unwrap().as_str(),
-            cargo_args.iter().chain(it).cloned().collect::<Vec<String>>())
-        },
-        None => ("NA", cargo_args),
-    };
 
     let target_triple = "mipsel-sony-psx";
-    let mut build = Command::new("cargo")
-        .arg("build")
-        .arg("-Z")
-        .arg("build-std=core,alloc")
-        .arg("--target")
-        .arg(target_triple)
-        // Change rustc and the linker to sensible defaults and make configuration easier
-        .env("RUSTC", "psx_rustc")
-        .env("RUSTFLAGS", "-C linker=../../mips_toolchain/ld")
-        .args(cargo_args)
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .spawn()
-        .unwrap();
+    if !skip_build {
+        let mut build = Command::new("cargo")
+            .arg("build")
+            .arg("-Z")
+            .arg("build-std=core,alloc")
+            .arg("--target")
+            .arg(target_triple)
+            // Change rustc and the linker to sensible defaults and make configuration easier
+            .env("RUSTC", "psx_rustc")
+            .env("RUSTFLAGS", "-C linker=../../mips_toolchain/ld")
+            .args(cargo_args)
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+            .unwrap();
 
-    let status = build.wait().unwrap();
-    if !status.success() {
-        let code = status.code().unwrap_or(1);
-        process::exit(code);
+        let status = build.wait().unwrap();
+        if !status.success() {
+            let code = status.code().unwrap_or(1);
+            process::exit(code);
+        }
     }
 
     let metadata = MetadataCommand::new()
@@ -57,8 +68,7 @@ fn main() {
             if target.kind.iter().any(|k| k == "bin") {
                 let elf = &target_dir.join(&target.name).to_str().unwrap().to_string();
                 let psexe = &format!("{}{}", &target.name, ".psexe");
-                let convert_args = vec![region, elf, psexe];
-                println!("{:#?}", convert_args);
+                let convert_args = vec![region.as_str(), elf, psexe];
                 elf2psexe::main(convert_args);
             }
         }
