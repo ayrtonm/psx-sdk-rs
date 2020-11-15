d84ee32b366fb36d8bd7f94c99ed5d03aaea71cd
Ayrton <a.munoz3327@gmail.com>
2020-11-15 12:49:17 -0400

    Got decompression working on x86
    Now moving to the psx should be as simple as integrating `decompress` with the
    psexe loader, though I might run into problems since the loader and the unzipped
    exe are in neighboring linker sections.

---
+++ examples/psexe_loader/decompress/src/main.rs
@@ -0,0 +1,24 @@
+use crate::huffman_code::{CODES, SYMBOLS};
+mod huffman_code;
+
+fn main() {
+    let compressed_exe = include_bytes!("../../rotating_square.psexe.hzip");
+    let mut exe = Vec::new();
+    let mut possible_code_len = 0;
+    let mut possible_code;
+    for w in compressed_exe.chunks(2) {
+        let mut stream = (w[0] as u32) | (w[1] as u32) << 8;
+        while stream != 0 {
+            stream <<= 1;
+            possible_code_len += 1;
+            possible_code = (stream >> 16) as u16 | (1 << possible_code_len);
+            CODES.iter().position(|&code| code == possible_code).map(|idx| {
+                let symbol = SYMBOLS[idx];
+                exe.push(symbol);
+                possible_code_len = 0;
+                stream &= 0x0000_ffff;
+            });
+        }
+    }
+    std::fs::write("../rotating_square.psexe.hunzip", exe).unwrap();
+}
