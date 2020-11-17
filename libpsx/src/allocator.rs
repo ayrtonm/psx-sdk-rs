fc3ea4db639cf5399f7541211a926e06f40fb33e
Ayrton <a.munoz3327@gmail.com>
2020-11-17 10:56:29 -0400

    Removed UB with allocator-specific interrupt::free
    Ideally I'd like to use interrupt::Mask for this type of thing, but it seems
    working with global_allocator may require me to be more flexible

--- libpsx/src/allocator.rs
+++ libpsx/src/allocator.rs
@@ -1,40 +1,37 @@
 use core::alloc::{GlobalAlloc, Layout};
 
 use crate::bios;
-use crate::interrupt;
 
-pub struct BiosAllocator {
-    int_mask: interrupt::Mask,
+pub struct BiosAllocator;
+
+// This is temporary, see if there's a way to libpsx::interrupt::Mask::free
+// instead w/o relying on UB
+mod interrupt {
+    pub fn free<F, R>(f: F) -> R
+    where F: FnOnce() -> R {
+        unsafe {
+            core::intrinsics::volatile_store(0x1F80_1074 as *mut u32, 0);
+        }
+        let ret = f();
+        unsafe {
+            core::intrinsics::volatile_store(0x1F80_1074 as *mut u32, 0x0000_07FF);
+        }
+        ret
+    }
 }
 
-// TODO: mutable transmutes are UB, see if there's a way around this without changing interrupt::Mask
-// Note `UnsafeCell` and `RefCell` probably won't work because they `impl !Sync` so they can't be in
-// statics and global allocators must be static. I might just end up rewriting interrupt::Mask::free
-// and directly using volatile_store
-#[warn(mutable_transmutes)]
 unsafe impl GlobalAlloc for BiosAllocator {
-    use core::mem::transmute;
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let int_mask = transmute::<_, &mut interrupt::Mask>(&self.int_mask);
-        int_mask.free(|| {
-            let ptr = bios::malloc(layout.size());
-            // TODO: test `ptr` for errors
-            ptr
-        })
+        interrupt::free(|| bios::malloc(layout.size()))
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        let int_mask = transmute::<_, &mut interrupt::Mask>(&self.int_mask);
-        int_mask.free(|| {
-            bios::free(ptr)
-        });
+        interrupt::free(|| bios::free(ptr))
     }
 }
 
 #[global_allocator]
-pub static HEAP: BiosAllocator = BiosAllocator {
-    int_mask: interrupt::Mask,
-};
+pub static HEAP: BiosAllocator = BiosAllocator;
 
 #[alloc_error_handler]
 fn on_oom(_layout: Layout) -> ! {
