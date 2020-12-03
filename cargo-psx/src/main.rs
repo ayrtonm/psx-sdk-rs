7f9fccdb25d977a2b2725aa098fc1a89e4a8945b
Ayrton <a.munoz3327@gmail.com>
2020-12-03 16:49:09 -0400

    Added support for cargo check in cargo-psx
    it's currently a flag, but it might make more sense to make it a normal arg
    e.g. cargo psx build
         cargo psx check
         cargo psx (defaults to build)

--- cargo-psx/src/main.rs
+++ cargo-psx/src/main.rs
@@ -59,7 +59,9 @@ fn main() {
         println!("  cargo psx [OPTIONS]\n");
         println!("OPTIONS:");
         println!("  --help, -h           Prints help information");
-        println!("  --toolchain <NAME>   Sets the name of the rustup toolchain to use (defaults to `psx`)");
+        println!(
+            "  --toolchain <NAME>   Sets the name of the rustup toolchain to use (defaults to `psx`)"
+        );
         println!("  --region <REGION>    Sets the game region to NA, EU or JP (default)");
         println!(
             "  --skip-build         Skips building and only packages an existing ELF into a PSEXE"
@@ -68,6 +70,7 @@ fn main() {
         println!("  --no-pad             Skips padding the PSEXE file size to a multiple of 0x800");
         println!("  --no-alloc           Avoids building the `alloc` crate");
         println!("  --lto                Enable link-time optimization and set codegen units to 1");
+        println!("  --check              Runs cargo check");
         println!("");
         println!("Run `cargo build -h` for build options");
         return
@@ -75,10 +78,11 @@ fn main() {
     let (region, cargo_args) = extract_key_value("--region", args);
     let (toolchain_name, cargo_args) = extract_key_value("--toolchain", cargo_args);
     let (skip_build, cargo_args) = extract_flag("--skip-build", cargo_args);
-    let (skip_pack, cargo_args) = extract_flag("--skip-pack", cargo_args);
+    let (mut skip_pack, cargo_args) = extract_flag("--skip-pack", cargo_args);
     let (no_pad, cargo_args) = extract_flag("--no-pad", cargo_args);
     let (no_alloc, cargo_args) = extract_flag("--no-alloc", cargo_args);
     let (lto, cargo_args) = extract_flag("--lto", cargo_args);
+    let (check, cargo_args) = extract_flag("--check", cargo_args);
 
     let region = region.unwrap_or("JP".to_string());
     let toolchain_name = toolchain_name.unwrap_or("psx".to_string());
@@ -94,11 +98,19 @@ fn main() {
     };
 
     let target_triple = "mipsel-sony-psx";
+
+    let cargo_subcmd = if check {
+        skip_pack = true;
+        "check"
+    } else {
+        "build"
+    };
+
     if !skip_build {
         // TODO: remove `RUSTFLAGS` env var after fixing rust-lld's alloc error
         let mut build = Command::new("cargo")
             .arg("+".to_string() + &toolchain_name)
-            .arg("build")
+            .arg(cargo_subcmd)
             .arg("-Z")
             .arg("build-std=".to_string() + &build_std)
             .arg("--target")
