b850255071117790277a8bb3aa5ba834de1c6ba2
Ayrton <a.munoz3327@gmail.com>
2021-04-14 14:48:27 -0500

    Added Read/Write impl for LZC cop2 regs

--- psx/src/hal/gte.rs
+++ psx/src/hal/gte.rs
@@ -11,3 +11,31 @@ read_only! {
     /// cop2r31 - LZCR - Count Leading Bits Result
     LZCR<u32>
 }
+
+impl<S: State> Read<u32> for LZCS<S> {
+    fn read(&self) -> u32 {
+        let lzcs;
+        unsafe {
+            asm!("mfc2 $2, $30", out("$2") lzcs);
+        }
+        lzcs
+    }
+}
+
+impl Write<u32> for LZCS<Mutable> {
+    fn write(&mut self, lzcs: u32) {
+        unsafe {
+            asm!("mtc2 $2, $30", in("$2") lzcs);
+        }
+    }
+}
+
+impl Read<u32> for LZCR {
+    fn read(&self) -> u32 {
+        let lzcr;
+        unsafe {
+            asm!("mfc2 $2, $30", out("$2") lzcr);
+        }
+        lzcr
+    }
+}
