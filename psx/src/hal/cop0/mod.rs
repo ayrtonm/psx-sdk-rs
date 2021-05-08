d911f33754adb94122d5cab1f80701c181b2462e
Ayrton <a.munoz3327@gmail.com>
2021-05-08 12:25:01 -0500

    Add remaining cop0 registers

--- psx/src/hal/cop0/mod.rs
+++ psx/src/hal/cop0/mod.rs
@@ -13,14 +13,28 @@ pub enum Mode {
 pub use status::IntMask;
 
 read_only_cop! {
-    /// cop0r14     - EPC - Return Address from Trap
-    EPC<u32>; COP: 0; R: 14
+    /// r8/BadVaddr - Bad virtual address
+    BadVaddr<u32>; COP: 0; R: 8,
+    /// r14/EPC - Return address from trap
+    EPC<u32>; COP: 0; R: 14,
+    /// r15/PRID - Processor ID
+    PRID<u32>; COP: 0; R: 15
 }
 
 read_write_cop! {
-    /// cop0r12     - SR - System status register
+    /// r3/BPC - Breakpoint on execute
+    BPC<u32>; COP: 0; R: 3,
+    /// r5/BDA - Breakpoint on data access
+    BDA<u32>; COP: 0; R: 5,
+    /// r7/DCIC - Breakpoint control
+    DCIC<u32>; COP: 0; R: 7,
+    /// r9/BDAM - Breakpoint on data access mask
+    BDAM<u32>; COP: 0; R: 9,
+    /// r11/BPCM - Execute breakpoint mask
+    BPCM<u32>; COP: 0; R: 11,
+    /// r12/SR - System status register
     Status<u32>; COP: 0; R: 12,
-    /// cop0r13     - CAUSE - Describes the most recently recognised exception
+    /// r13/CAUSE - Describes the most recently recognised exception
     Cause<u32>; COP: 0; R: 13
 }
 
