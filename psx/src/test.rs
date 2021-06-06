25e63e3cd2b16dabccf7b4aa7bee45ad08b786eb
Ayrton <a.munoz3327@gmail.com>
2021-06-06 17:48:52 -0500

    Change test randomization to use const_random!
    This removes the BIOS dependency for generating random numbers and storing the
    random seed in the memory card for different runs. const_random! isn't ideal
    since it produces a random constant at each invocation site so it won't behave
    as expected when used in a loop inside a const fn, but with some slight
    workarounds to the original method for generating idx params it works.

--- psx/src/test.rs
+++ psx/src/test.rs
@@ -1,10 +1,8 @@
 #![cfg(test)]
 use crate::bios;
-use crate::bios::fs::{File, FileTy, MemCard, SeekFrom};
-use crate::bios::kernel;
 use crate::hal::{MutRegister, Mutable, Register, GP1, I_MASK, I_STAT};
+use const_random::const_random;
 use core::any::type_name;
-use core::convert::TryInto;
 use num::integer::gcd;
 
 pub trait Test {
@@ -19,86 +17,53 @@ impl<T: Fn()> Test for T {
     }
 }
 
-pub fn runner(tests: &[&dyn Test]) {
-    if cfg!(not(feature = "deterministic_test_runner")) {
-        // Initialize RNG seed
-        unsafe {
-            kernel::init_card(true);
-            kernel::start_card();
-        }
-        let file_name = "bu00:\\SEED";
-        let mut buf = [0; MemCard::SECTOR_SIZE];
-        let (mut file, seed) = match File::open(file_name) {
-            Ok(file) => {
-                // Read previous seed
-                file.read(&mut buf).ok();
-                // Unwrap will not fail because subslice contains exactly 4 indices
-                let seed = unsafe { u32::from_ne_bytes(buf[0..4].try_into().unwrap_unchecked()) };
-                (file, seed)
-            },
-            Err(_) => {
-                let file =
-                    File::create(file_name, None).expect("Unable to create memory card file");
-                let initial_seed = 0xdead_beef;
-                (file, initial_seed)
-            },
-        };
-        bios::srand(seed);
-        // Compute seed for the next run
-        let next_seed =
-            bios::rand() as u32 | ((bios::rand() as u32) << 15) | ((bios::rand() as u32) << 30);
-        // Write next seed to the start of the file
-        buf[0..4].copy_from_slice(&next_seed.to_ne_bytes());
-        file.seek(SeekFrom::Start(0)).ok();
-        file.write(&buf).ok();
-        unsafe {
-            kernel::stop_card();
-        }
+/// Returns parameters (a, b) where a is coprime with NUM_TESTS and both are
+/// smaller than NUM_TESTS.
+fn idx_params<const NUM_TESTS: usize>() -> (usize, usize) {
+    let b = const_random!(usize) % NUM_TESTS;
+    let mut a = const_random!(usize);
+    while gcd(a, NUM_TESTS) != 1 {
+        // Invoking const_random! in a loop would produce the same number even if
+        // this were a const fn assured to be evaluated at compile-time
+        a = a / gcd(a, NUM_TESTS);
     }
+    (a % NUM_TESTS, b)
+}
 
+pub fn runner<const NUM_TESTS: usize>(tests: &[&dyn Test; NUM_TESTS]) {
     // Compute parameters for randomly iterating through tests
-    let num_tests = tests.len();
-    // Expand random numbers to 32 bits to avoid overflow when calculating index
     let (a, b) = if cfg!(not(feature = "deterministic_test_runner")) {
-        let b = bios::rand() as usize % num_tests;
-        let mut a = bios::rand() as usize;
-        // This ensures that step size `a` and the number of tests are coprime
-        // TODO: Will this always terminate?
-        while gcd(a, num_tests) != 1 {
-            a = bios::rand() as usize;
-        }
-        (a, b)
+        idx_params::<NUM_TESTS>()
     } else {
         (0, 0)
     };
-    // All tests will run assuming no overflow happens when computing the random
-    // indices. The sanity check stores the sum of all indices to detect most of
-    // these subtle overflow bugs in the test runner
-    let mut sanity_check: usize = 0;
+    // Sanity check to ensure that randomizing tests doesn't run a given test more
+    // than once
+    let mut idx_count: usize = 0;
 
-    println!("running {} tests", tests.len());
-    for n in 0..num_tests {
+    println!("running {} tests", NUM_TESTS);
+    for n in 0..NUM_TESTS {
         bios::critical_section(|| {
             I_MASK::<Mutable>::load().disable_all().store();
             I_STAT::<Mutable>::load().ack_all().store();
             GP1.reset_gpu();
             let idx = if cfg!(not(feature = "deterministic_test_runner")) {
-                let idx = (a * n + b) % num_tests;
-                sanity_check += idx;
-                idx
+                (a * n + b) % NUM_TESTS
             } else {
                 n
             };
+            idx_count += idx;
             tests[idx].run();
         });
     }
     if cfg!(not(feature = "deterministic_test_runner")) {
         assert!(
-            sanity_check == (num_tests * (num_tests - 1)) / 2,
-            "Sum of random indices differs from expected value, re-run tests without randomization"
+            idx_count == (NUM_TESTS * (NUM_TESTS - 1)) / 2,
+            "Error detected in test framework! Double check test runner or\
+             re-run tests without randomization"
         );
     }
     // Failing tests panic and unwinding will add unnecessary bloat to the binary
     // so the following line is only displayed if all tests pass
-    println!("\ntest result: ok. {} passed; 0 failed\n", tests.len());
+    println!("\ntest result: ok. {} passed; 0 failed\n", NUM_TESTS);
 }
