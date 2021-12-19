50bd0cc7b01454513e52d66e94c3bf7f4ef23e38
Ayrton <a.munoz3327@gmail.com>
2021-12-19 02:24:00 -0500

    Add psx::dma::otc

---
+++ psx/src/dma/otc.rs
@@ -0,0 +1,23 @@
+use crate::Result;
+use crate::dma::Channel;
+use crate::hw::dma::otc::{Address, Block, Control};
+
+pub struct OTC(Channel<Address, Block, Control>);
+
+impl OTC {
+    pub fn new() -> Self {
+        OTC(Channel::new())
+    }
+
+    pub fn control(&mut self) -> &mut Control {
+        &mut self.0.control
+    }
+
+    pub fn send(&mut self, block: &[u32]) -> Result<()> {
+        self.0.send_and(block, || ())
+    }
+
+    pub fn send_and<F: FnOnce() -> R, R>(&mut self, block: &[u32], f: F) -> Result<R> {
+        self.0.send_and(block, f)
+    }
+}
