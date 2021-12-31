7aa78c11b183a4433801ea96933eea803e76191e
Ayrton <a.munoz3327@gmail.com>
2021-12-31 02:48:10 -0500

    Make Gamepad::pressed use read_volatile
    I'm still having some issues without LTO

--- psx/src/sys/gamepad.rs
+++ psx/src/sys/gamepad.rs
@@ -2,6 +2,7 @@
 
 use crate::sys::{critical_section, kernel};
 use core::marker::PhantomData;
+use core::ptr::read_volatile;
 use strum_macros::IntoStaticStr;
 
 pub const BUFFER_SIZE: usize = 0x22;
@@ -37,8 +38,8 @@ pub enum Error {
 
 #[derive(Debug)]
 pub struct Gamepad<'a, 'b> {
-    buf0: *mut u8,
-    buf1: *mut u8,
+    buf0_ptr: *mut u8,
+    buf1_ptr: *mut u8,
     _buf0: PhantomData<&'a ()>,
     _buf1: PhantomData<&'b ()>,
 }
@@ -47,8 +48,8 @@ impl<'a, 'b> Gamepad<'a, 'b> {
     pub fn new(
         buf0: &'a mut [u8; BUFFER_SIZE], buf1: &'b mut [u8; BUFFER_SIZE],
     ) -> Result<Self, Error> {
-        let buf0 = buf0.as_mut_ptr();
-        let buf1 = buf1.as_mut_ptr();
+        let buf0_ptr = buf0.as_mut_ptr();
+        let buf1_ptr = buf1.as_mut_ptr();
         critical_section(|| unsafe {
             if PAD_INITIALIZED {
                 return Err(Error::AlreadyInitialized)
@@ -58,18 +59,18 @@ impl<'a, 'b> Gamepad<'a, 'b> {
         })?;
         unsafe {
             kernel::start_pad();
-            kernel::init_pad(buf0, BUFFER_SIZE, buf1, BUFFER_SIZE);
+            kernel::init_pad(buf0_ptr, BUFFER_SIZE, buf1_ptr, BUFFER_SIZE);
         }
         Ok(Gamepad {
-            buf0,
-            buf1,
+            buf0_ptr,
+            buf1_ptr,
             _buf0: PhantomData,
             _buf1: PhantomData,
         })
     }
 
     pub fn released(&self, button: Button) -> bool {
-        let val = unsafe { *self.buf0.add(2).cast::<u16>() };
+        let val = unsafe { read_volatile(self.buf0_ptr.add(2).cast::<u16>()) };
         val & (1 << (button as u16)) != 0
     }
 
