c0199b642d8186beb48a1e8c2a0ba9fc77067b5f
Ayrton <a.munoz3327@gmail.com>
2022-01-02 11:49:23 -0500

    Add GTE matrix definitions

--- psx/src/gte/mod.rs
+++ psx/src/gte/mod.rs
@@ -1,28 +1,73 @@
+use crate::hw::gte::{VectorXY, VectorZ};
+use crate::hw::gte::{VXY0, VXY1, VXY2, VZ0, VZ1, VZ2};
+use crate::hw::gte::{MatrixAB, MatrixC};
+use crate::hw::gte::{RT11_12, RT13_21, RT22_23, RT31_32, RT33};
+use crate::hw::gte::{L11_12, L13_21, L22_23, L31_32, L33};
+use crate::hw::gte::{LR11_12, LR13_21, LR22_23, LR31_32, LR33};
 use crate::hw::Register;
-use crate::hw::gte::{VXY0, VZ0};
 
-pub struct V3 {
-    vxy: VXY0,
-    vz: VZ0,
+pub struct Vector<VXY: VectorXY, VZ: VectorZ> {
+    vxy: VXY,
+    vz: VZ,
 }
 
-impl V3 {
+pub type V0 = Vector<VXY0, VZ0>;
+pub type V1 = Vector<VXY1, VZ1>;
+pub type V2 = Vector<VXY2, VZ2>;
+
+impl<VXY: VectorXY, VZ: VectorZ> Vector<VXY, VZ> {
+    /// Creates handles to a GTE vector. This caches the input values, but the
+    /// load is deferred until the vector is used.
     pub fn new([x, y, z]: [i16; 3]) -> Self {
         let x = x as u32;
         let y = y as u32;
-        let mut vxy = VXY0::skip_load();
+        let mut vxy = VXY::skip_load();
         vxy.assign(x | y << 16);
-        let mut vz = VZ0::skip_load();
+        let mut vz = VZ::skip_load();
         vz.assign(z);
-        Self {
-            vxy,
-            vz
-        }
+        Self { vxy, vz }
     }
 
-    pub fn use(&mut self) {
-        self.vxy.store();
-        self.vz.store();
-        // gte cmd goes here
+    //pub fn use(&mut self) {
+    //    self.vxy.store();
+    //    self.vz.store();
+    //    // gte cmd goes here
+    //}
+}
+
+pub struct Matrix<AB: MatrixAB, CD: MatrixAB, EF: MatrixAB, GH: MatrixAB, I: MatrixC> {
+    ab: AB,
+    cd: CD,
+    ef: EF,
+    gh: GH,
+    i: I,
+}
+
+pub type RT = Matrix<RT11_12, RT13_21, RT22_23, RT31_32, RT33>;
+pub type LLM = Matrix<L11_12, L13_21, L22_23, L31_32, L33>;
+pub type LCM = Matrix<LR11_12, LR13_21, LR22_23, LR31_32, LR33>;
+
+impl<AB: MatrixAB, CD: MatrixAB, EF: MatrixAB, GH: MatrixAB, I: MatrixC> Matrix<AB, CD, EF, GH, I> {
+    /// Creates handles to a GTE matrix. This caches the input values, but the
+    /// load is deferred until the matrix is used.
+    pub fn new([r1, r2, r3]: [[i16; 3]; 3]) -> Self {
+        let [r11, r12, r13] = r1.map(|r| r as u32);
+        let [r21, r22, r23] = r2.map(|r| r as u32);
+        let [r31, r32, r33] = r3;
+        let r31 = r31 as u32;
+        let r32 = r32 as u32;
+        let mut ab = AB::skip_load();
+        ab.assign(r11 | r12 << 16);
+        let mut cd = CD::skip_load();
+        cd.assign(r13 | r21 << 16);
+        let mut ef = EF::skip_load();
+        ef.assign(r22 | r23 << 16);
+        let mut gh = GH::skip_load();
+        gh.assign(r31 | r32 << 16);
+        let mut i = I::skip_load();
+        i.assign(r33);
+        Self {
+            ab, cd, ef, gh, i
+        }
     }
 }
--- psx/src/hw/gte/mod.rs
+++ psx/src/hw/gte/mod.rs
@@ -25,4 +25,69 @@ define_cop! {
     LZCS<u32>; COP: 2; R: 30,
     /// Leading zeros count result
     LZCR<u32>; COP: 2; R: 31,
+
+    /// Rotation matrix entries RT11 and RT12
+    RT11_12<u32>; COP: 2; R: 32,
+    /// Rotation matrix entries RT13 and RT21
+    RT13_21<u32>; COP: 2; R: 33,
+    /// Rotation matrix entries RT22 and RT23
+    RT22_23<u32>; COP: 2; R: 34,
+    /// Rotation matrix entries RT31 and RT32
+    RT31_32<u32>; COP: 2; R: 35,
+    /// Rotation matrix entry RT33
+    RT33<i16>;    COP: 2; R: 36,
+
+    /// Light matrix entries L11 and L12
+    L11_12<u32>; COP: 2; R: 40,
+    /// Light matrix entries L13 and L21
+    L13_21<u32>; COP: 2; R: 41,
+    /// Light matrix entries L22 and L23
+    L22_23<u32>; COP: 2; R: 42,
+    /// Light matrix entries L31 and L32
+    L31_32<u32>; COP: 2; R: 43,
+    /// Light matrix entry L33
+    L33<i16>;    COP: 2; R: 44,
+
+    /// Light color matrix entries LR11 and LR12
+    LR11_12<u32>; COP: 2; R: 48,
+    /// Light color matrix entries LR13 and LR21
+    LR13_21<u32>; COP: 2; R: 49,
+    /// Light color matrix entries LR22 and LR23
+    LR22_23<u32>; COP: 2; R: 50,
+    /// Light color matrix entries LR31 and LR32
+    LR31_32<u32>; COP: 2; R: 51,
+    /// Light color matrix entry LR33
+    LR33<i16>; COP: 2; R: 52,
 }
+
+pub trait VectorXY: Register<u32> {}
+pub trait VectorZ: Register<i16> {}
+
+pub trait MatrixAB: Register<u32> {}
+pub trait MatrixC: Register<i16> {}
+
+impl VectorXY for VXY0 {}
+impl VectorXY for VXY1 {}
+impl VectorXY for VXY2 {}
+
+impl VectorZ for VZ0 {}
+impl VectorZ for VZ1 {}
+impl VectorZ for VZ2 {}
+
+impl MatrixAB for RT11_12 {}
+impl MatrixAB for RT13_21 {}
+impl MatrixAB for RT22_23 {}
+impl MatrixAB for RT31_32 {}
+impl MatrixC for RT33 {}
+
+impl MatrixAB for L11_12 {}
+impl MatrixAB for L13_21 {}
+impl MatrixAB for L22_23 {}
+impl MatrixAB for L31_32 {}
+impl MatrixC for L33 {}
+
+impl MatrixAB for LR11_12 {}
+impl MatrixAB for LR13_21 {}
+impl MatrixAB for LR22_23 {}
+impl MatrixAB for LR31_32 {}
+impl MatrixC for LR33 {}
