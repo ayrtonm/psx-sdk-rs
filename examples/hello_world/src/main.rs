a69a29ab5ef056f04ff243f33d128798d9c4c554
Ayrton <a.munoz3327@gmail.com>
2022-06-01 23:46:25 -0400

    Remove broken demos and simplify hello_world

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,30 +1,18 @@
 #![no_std]
 #![no_main]
 
-use psx::gpu::colors::*;
-use psx::gpu::Vertex;
-use psx::{dprintln, Font, Framebuffer};
+use psx::{dprintln, Framebuffer};
 
 #[no_mangle]
 fn main() {
-    let buf0 = Vertex(0, 0);
-    let buf1 = Vertex(0, 240);
-    let res = Vertex(320, 240);
-    // Creating a new Framebuffer will `Err` if the resolution isn't valid.
-    let mut fb = Framebuffer::new(buf0, buf1, res).expect("Invalid resolution");
-    let offset = Vertex(0, 8);
-    let mut text_box = Font::default().text_box(offset);
-    dprintln!(text_box, "Hello, world!");
-    text_box.newline();
-    dprintln!(text_box, "I can write in all these colors");
-    let colors = [
-        RED, ORANGE, YELLOW, LIME, GREEN, MINT, CYAN, AQUA, BLUE, INDIGO, VIOLET, PINK,
-    ];
-    for c in colors {
-        text_box.change_color(c);
-        dprintln!(text_box, "{:?}", c);
-    }
-    // Swap the frambuffers with the CPU instead of the GPU DMA channel
-    fb.swap(None);
+    let buf0 = (0, 0);
+    let buf1 = (0, 240);
+    let res = (320, 240);
+    let txt_offset = (0, 8);
+    let mut fb = Framebuffer::new(buf0, buf1, res, None).unwrap();
+    let font = fb.load_default_font();
+    let mut txt = font.new_text_box(txt_offset, res);
+    dprintln!(txt, "Hello, world!");
+    fb.swap();
     loop {}
 }
