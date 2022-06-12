a5e9dd0104430c6529688698f7d5e575cbb2c4fb
Ayrton <a.munoz3327@gmail.com>
2022-06-12 15:40:43 -0400

    Add padding to include_words!

--- psx/src/macros.rs
+++ psx/src/macros.rs
@@ -7,22 +7,27 @@ macro_rules! file_size {
     }};
 }
 
-/// Includes the specified file as a `&mut [u32; N]`. This is exactly like
-/// `include_bytes`, expect that it checks the size is a multiple of 4 bytes at
-/// compile-time to allow creating a mutable `u32` slice rather than a `u8`
-/// slice.
+/// Includes the specified file as a `&mut [u32; N]`.
+///
+/// This is exactly like `include_bytes`, expect that it pads the size to a
+/// multiple of 4 bytes to allow creating a mutable `u32` slice rather than a
+/// `u8` slice.
 #[macro_export]
 macro_rules! include_words {
     ($file:literal) => {{
-        const N: usize = $crate::file_size!($file) / 4;
-        const _: () = {
-            if N % 4 != 0 {
-                panic!("File size is not a multiple of 4 bytes");
-            }
-        };
+        const N: usize = ($crate::file_size!($file) + 3) / 4;
         #[used]
         #[link_section = ".data"]
-        static mut RET: [u32; N] = unsafe { core::mem::transmute(*include_bytes!($file)) };
+        static mut RET: [u32; N] = {
+            let input_file = *include_bytes!($file);
+            let mut padded_data = [0u8; N * 4];
+            let mut i = 0;
+            while i < input_file.len() {
+                padded_data[i] = input_file[i];
+                i += 1;
+            }
+            unsafe { core::mem::transmute(padded_data) }
+        };
         unsafe { &mut RET }
     }};
 }
