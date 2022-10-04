5fa9b54b65ddf295d662e9e194a72f3134d9467f
Ayrton <a.munoz3327@gmail.com>
2022-10-04 02:29:36 -0400

    Add BIOS system version/date functions

--- psx/src/sys/mod.rs
+++ psx/src/sys/mod.rs
@@ -2,6 +2,8 @@
 //!
 //! This module contains wrappers for functions provided by the BIOS.
 
+use core::ffi::CStr;
+
 pub mod fs;
 pub mod gamepad;
 pub mod heap;
@@ -21,4 +23,19 @@ pub fn critical_section<F: FnOnce() -> R, R>(f: F) -> R {
             f()
         }
     }
+}
+
+/// Returns the kernel's version string.
+pub fn get_system_version() -> &'static CStr {
+    // SAFETY: Calling get_system_info with index 2 gives a pointer with a
+    // static lifetime to the version string. There are no safety requirement.
+    let version = unsafe { kernel::psx_get_system_info(2) as *const i8 };
+    // SAFETY: Let's hope the BIOS returned a pointer to a null-terminated string
+    // to its own memory.
+    unsafe { CStr::from_ptr(version) }
+}
+
+/// Returns the kernel's date in BCD (e.g. 0x19951204).
+pub fn get_system_date() -> u32 {
+    unsafe { kernel::psx_get_system_info(0) }
 }
