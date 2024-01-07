c04565a5ce63d3802672d99a13cb366ae56416a0
Ayrton <a.munoz3327@gmail.com>
2024-01-06 19:19:44 -0500

    psx: Remove loop after nested panic warning and refactor panic.rs
    In case of nested panics (e.g. cause by the CStr limit in println) this allows
    execution to continue until the second panic message.

--- psx/src/panic.rs
+++ psx/src/panic.rs
@@ -1,81 +1,80 @@
-use crate::sys;
+use crate::hw::{cop0, Register};
 use crate::{dprintln, println, Framebuffer};
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    // SAFETY: This should not return so we don't care about handling nested
-    // exceptions properly
-    unsafe {
-        sys::critical_section(|_| {
-            // SAFETY: We're in a critical section so no other threads can get a
-            // mutable reference. The previous (nested) call to panic, if any,
-            // that accesses IN_PANIC won't hold onto a reference since we drop
-            // them before calling display_panic.
-            static mut IN_PANIC: bool = false;
+    cop0::Status::new().critical_section(|_| {
+        // SAFETY: We're in a critical section so no other threads can get a
+        // mutable reference. The previous (nested) call to panic, if any,
+        // that accesses IN_PANIC won't hold onto a reference since we drop
+        // them before calling display_panic.
+        static mut IN_PANIC: bool = false;
+        unsafe {
             if IN_PANIC {
                 println!("Panicked in panic!");
-                loop {}
             }
             IN_PANIC = true;
-            display_panic(info)
-        })
-    }
+        }
+        display_panic(info);
+        loop {}
+    })
 }
 
-fn display_panic(info: &core::panic::PanicInfo) -> ! {
+fn display_panic(info: &core::panic::PanicInfo) {
     // Print to stdout unless no_panic is set. This includes the default case since
     // printing to the screen during a panic is not always reliable.
     #[cfg(not(feature = "no_panic"))]
-    {
+    min_panic(info);
+    // In the default case print the panic message to the screen
+    #[cfg(not(any(feature = "min_panic", feature = "no_panic")))]
+    normal_panic(info);
+}
+
+fn min_panic(info: &core::panic::PanicInfo) {
+    match info.location() {
+        Some(location) => {
+            println!(
+                "Panicked at {}:{}:{}",
+                location.file(),
+                location.line(),
+                location.column()
+            )
+        },
+        None => {
+            println!("Panicked at unknown location")
+        },
+    }
+    if let Some(msg) = info.message() {
+        println!("{}", msg)
+    }
+}
+
+fn normal_panic(info: &core::panic::PanicInfo) {
+    // We have no idea what state the GPU was in when the panic happened, so reset
+    // it to a known state and reload the font into VRAM.
+    let mut fb = Framebuffer::default();
+    let mut txt = fb.load_default_font().new_text_box((0, 8), (320, 240));
+    loop {
+        txt.reset();
         match info.location() {
             Some(location) => {
-                println!(
+                dprintln!(
+                    txt,
                     "Panicked at {}:{}:{}",
                     location.file(),
                     location.line(),
                     location.column()
-                )
+                );
             },
             None => {
-                println!("Panicked at unknown location")
+                dprintln!(txt, "Panicked at unknown location");
             },
         }
         if let Some(msg) = info.message() {
-            println!("{}", msg)
+            dprintln!(txt, "{}", msg);
         }
-    };
-    // In the default case print the panic message to the screen
-    #[cfg(not(any(feature = "min_panic", feature = "no_panic")))]
-    {
-        // We have no idea what state the GPU was in when the panic happened, so reset
-        // it to a known state and reload the font into VRAM.
-        let mut fb = Framebuffer::default();
-        let mut txt = fb.load_default_font().new_text_box((0, 8), (320, 240));
-        loop {
-            txt.reset();
-            match info.location() {
-                Some(location) => {
-                    dprintln!(
-                        txt,
-                        "Panicked at {}:{}:{}",
-                        location.file(),
-                        location.line(),
-                        location.column()
-                    );
-                },
-                None => {
-                    dprintln!(txt, "Panicked at unknown location");
-                },
-            }
-            if let Some(msg) = info.message() {
-                dprintln!(txt, "{}", msg);
-            }
-            fb.draw_sync();
-            fb.wait_vblank();
-            fb.swap();
-        }
-    };
-    // Both min_panic and no_panic end execution here
-    #[cfg(any(feature = "min_panic", feature = "no_panic"))]
-    loop {}
+        fb.draw_sync();
+        fb.wait_vblank();
+        fb.swap();
+    }
 }
