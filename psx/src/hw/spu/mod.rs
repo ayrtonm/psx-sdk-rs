eb89b40df555a7b1663776d7675ab498017de7b3
techmetx11 <techmetx11@disroot.org>
2026-07-02 22:42:50 +0100

    Add code for the SPU's ADSR, and move envelope structs from volume.rs to adsr.rs
    Comments are not yet complete

--- psx/src/hw/spu/mod.rs
+++ psx/src/hw/spu/mod.rs
@@ -1,5 +1,6 @@
 //! SPU interface
 
+pub mod adsr;
 pub mod reverb;
 pub mod volume;
 
--- psx/src/hw/spu/volume.rs
+++ psx/src/hw/spu/volume.rs
@@ -1,43 +1,49 @@
 //! SPU volume module
 
-/// Volume sweep mode.
-#[derive(Debug, Clone, Copy)]
-#[repr(u8)]
-pub enum SweepMode {
-    /// The volume sweeps linearly
-    Linear = 0,
-    /// The volume sweeps exponentially
-    Exponential = 1,
-}
-
-/// Volume direction sweep
-#[derive(Debug, Clone, Copy)]
-#[repr(u8)]
-pub enum SweepDirection {
-    /// The volume increases up to `+0x7FFF`
-    Increase = 0,
-    /// The volume decreases down to `0x0000`
-    Decrease = 1,
-}
-
-/// Volume sweep phase
-#[derive(Debug, Clone, Copy)]
-#[repr(u8)]
-pub enum SweepPhase {
-    /// The volume sweeps normally (increase => +0x7FFF, decrease => 0x0000)
-    Positive = 0,
-    /// The volume sweeps from the negative sign (increase => -0x7FFF, decrease
-    /// => 0x0000)
-    Negative = 1,
-}
+use crate::hw::spu::adsr::{EnvelopeDirection, EnvelopeMode, EnvelopePhase};
 
 /// The volume sweep structure.
 pub struct VolumeSweep {
-    mode: SweepMode,
-    direction: SweepDirection,
-    phase: SweepPhase,
-    shift: u8,
-    step: u8,
+    value: u16,
+}
+
+impl VolumeSweep {
+    /// The volume sweep mode.
+    pub fn mode(&self) -> EnvelopeMode {
+        match (self.value >> 14) & 1 {
+            0 => EnvelopeMode::Linear,
+            1 => EnvelopeMode::Exponential,
+            _ => unreachable!(),
+        }
+    }
+
+    /// The volume sweep direction.
+    pub fn direction(&self) -> EnvelopeDirection {
+        match (self.value >> 13) & 1 {
+            0 => EnvelopeDirection::Increase,
+            1 => EnvelopeDirection::Decrease,
+            _ => unreachable!(),
+        }
+    }
+
+    /// The volume sweep phase.
+    pub fn phase(&self) -> EnvelopePhase {
+        match (self.value >> 12) & 1 {
+            0 => EnvelopePhase::Positive,
+            1 => EnvelopePhase::Negative,
+            _ => unreachable!(),
+        }
+    }
+
+    /// The volume sweep shift.
+    pub fn shift(&self) -> u8 {
+        ((self.value >> 2) & 0x1F) as u8
+    }
+
+    /// The volume sweep step.
+    pub fn step(&self) -> u8 {
+        (self.value & 3) as u8
+    }
 }
 
 /// SPU volume setting.
@@ -48,46 +54,12 @@ pub enum Volume {
     Sweep(VolumeSweep),
 }
 
-fn parse_sweep(value: u16) -> VolumeSweep {
-    VolumeSweep {
-        mode: match (value >> 14) & 1 {
-            0 => SweepMode::Linear,
-            1 => SweepMode::Exponential,
-            _ => unreachable!(),
-        },
-
-        direction: match (value >> 13) & 1 {
-            0 => SweepDirection::Increase,
-            1 => SweepDirection::Decrease,
-            _ => unreachable!(),
-        },
-
-        phase: match (value >> 12) & 1 {
-            0 => SweepPhase::Positive,
-            1 => SweepPhase::Negative,
-            _ => unreachable!(),
-        },
-
-        shift: ((value >> 2) & 0x1F) as u8,
-        step: (value & 3) as u8,
-    }
-}
-
-fn encode_sweep(sweep: &VolumeSweep) -> u16 {
-    0x8000 |
-        ((sweep.mode as u16) << 14) |
-        ((sweep.direction as u16) << 13) |
-        ((sweep.phase as u16) << 12) |
-        ((sweep.shift as u16) << 2) |
-        (sweep.step as u16)
-}
-
 impl From<u16> for Volume {
     fn from(value: u16) -> Self {
         match value >> 15 {
             // Sign-extend the 15-bit signed volume
             0 => Volume::Normal(((value << 1) as i16) >> 1),
-            1 => Volume::Sweep(parse_sweep(value)),
+            1 => Volume::Sweep(VolumeSweep { value }),
             _ => unreachable!(),
         }
     }
@@ -97,7 +69,7 @@ impl From<&Volume> for u16 {
     fn from(val: &Volume) -> Self {
         match val {
             Volume::Normal(vol) => (*vol as u16) & 0x7FFF,
-            Volume::Sweep(sweep) => encode_sweep(sweep),
+            Volume::Sweep(sweep) => sweep.value,
         }
     }
 }
